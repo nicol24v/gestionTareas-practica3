d969debb85fea71f487a642235524cb235790e3f
Nicole Vizuete Personal <vizuetenicol@gmail.com>
2026-06-23 14:18:07 -0500

    segundo envio

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase, Client
+from .models import Tarea
+import json
+
+class TareaModelTest(TestCase):
+    def test_crear_tarea(self):
+        tarea = Tarea.objects.create(titulo='Test tarea')
+        self.assertEqual(tarea.titulo, 'Test tarea')
+        self.assertEqual(tarea.estado, 'pendiente')
+
+    def test_str_tarea(self):
+        tarea = Tarea.objects.create(titulo='Mi tarea')
+        self.assertEqual(str(tarea), 'Mi tarea')
+
+class TareaAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_listar_tareas_vacio(self):
+        response = self.client.get('/tareas/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_crear_tarea_api(self):
+        response = self.client.post(
+            '/tareas/crear/',
+            data=json.dumps({'titulo': 'Nueva tarea'}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 201)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Tarea
+
+def listar_tareas(request):
+    tareas = list(Tarea.objects.values())
+    return JsonResponse({'tareas': tareas})
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def crear_tarea(request):
+    data = json.loads(request.body)
+    tarea = Tarea.objects.create(
+        titulo=data['titulo'],
+        descripcion=data.get('descripcion', ''),
+    )
+    return JsonResponse({'id': tarea.id, 'titulo': tarea.titulo}, status=201)
